9229a1bb0cbbcaa54e15807de23951e1aabe9007
ruthjomo <ruthjomo19@gmail.com>
2020-11-12 16:09:02 +0300

    added changes in the file

--- src/app/men/men.component.ts
+++ src/app/men/men.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./men.component.css']
 })
 export class MenComponent implements OnInit {
-                  
-
   constructor() { }
 
   ngOnInit(): void {
