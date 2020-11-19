9f0b8a5e039ab5a77fa3b456436038adfa567f5c
msyoki <msyokimutua@gmail.com>
2020-11-19 09:58:01 +0300

    update cat search api url

--- src/app/men/men-products.service.ts
+++ src/app/men/men-products.service.ts
@@ -7,7 +7,7 @@ import { Product } from '../landing-page/products';
   providedIn: 'root'
 })
 export class MenProductsService {
-  API_URL = 'http://127.0.0.1:8000/api';
+  API_URL = 'https://dressup05.herokuapp.com/api';
 
   constructor(private http: HttpClient) { }
 
