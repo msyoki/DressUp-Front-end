8c08da9a57a271be7e7791fbab73c358d0f251c5
ruthjomo <ruthjomo19@gmail.com>
2020-11-24 14:17:32 +0300

    added more changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoginService } from '../login/login.service'
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  title = 'DressUp';  
-
-  constructor() { }
-
+  title = 'DressUp';
+  constructor(private loginService:LoginService,private router: Router) { }
   ngOnInit(): void {
   }
-
+  logout(){
+    this.loginService.logout();
+    Swal.fire({
+      icon: 'success',
+      text: ' logged out!'
+    })
+    this.router.navigate(["login"])
+  }
 }
