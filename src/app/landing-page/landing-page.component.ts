7095b279e2b6b686527d593dbbe0f49f6c691f13
Msyoki <66805330+msyoki@users.noreply.github.com>
2020-11-25 22:59:38 +0300

    Update logout route

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -23,7 +23,7 @@ export class LandingPageComponent implements OnInit {
       icon: 'success',  
       text: ' logged out!'
     })
-    this.router.navigate(["home"]) 
+    this.router.navigate([""]) 
     
   }
 
