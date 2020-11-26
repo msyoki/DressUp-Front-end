c8d788d0f3bc197bfcd89628ab9a5942c7b47241
msyoki <msyokimutua@gmail.com>
2020-11-26 08:16:10 +0300

    update category navbar

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -23,7 +23,7 @@ export class LandingPageComponent implements OnInit {
       icon: 'success',  
       text: ' logged out!'
     })
-    this.router.navigate([""]) 
+    // this.router.navigate(["login"]) 
     
   }
 
